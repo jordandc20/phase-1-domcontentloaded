2c75b554d56bb17c8170ee1da55d5d5f8d6ef46c
Diana Jordan <dianacjordan@gmail.com>
2023-05-05 16:27:12 -0400

    finish

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function (e) {
-    
+
 
     e.target.querySelector('#text').textContent = 'This is really cool!'
 }
 )
 
 
+function counter (start){
+let i=start
+while (i>=0) {
+console.log(i)
+i--
+
+}
+}
